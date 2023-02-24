1263542b185d804f2dd8467acd0373f6af1cbf08
Greg_Broflovski <ggherdelescu@mail.ru>
2023-02-24 17:35:15 +0200

    Made paragraph component

--- src/components/Header/header.stories.tsx
+++ src/components/Header/header.stories.tsx
@@ -1,13 +1,12 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { Header as CustomHeader } from ".";
-import { UserAccountIcon } from "../../public/icons/UserAccountIcon";
 
 export default {
   title: "Components/Header",
   parameters: {
     layout: "fullscreen",
-  }
+  },
 } as ComponentMeta<typeof CustomHeader>;
 
 export const Header: ComponentStory<typeof CustomHeader> = () => (
